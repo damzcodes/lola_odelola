7d2cd797e59b1cc1f170ddb778ac01722145ee17
Lola <damzcodes@gmail.com>
2016-10-25 17:39:25 +0100

    trying to do bloody particles.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/' => 'static_pages#index'
+
+  get 'about' => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
