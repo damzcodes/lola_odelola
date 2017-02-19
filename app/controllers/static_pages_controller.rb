4c7b2deac670c422b6d081daa0636d529237e403
damzcodes <damzcodes@gmail.com>
2017-02-19 10:09:20 +0000

    add talks page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def hire_me
   end
+
+  def talks
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'static_pages#about'
   get 'hire_me' => 'static_pages#hire_me'
+  get 'talks' => 'static_pages#talks'
 
   get 'contact' => 'static_pages#contact'
 
