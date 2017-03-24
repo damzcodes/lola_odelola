ac1d5b2a20cb2f0a78c747633d06b56fa7458497
damzcodes <damzcodes@gmail.com>
2017-03-24 14:05:59 +0000

    fix pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,17 @@
 class PostsController < ApplicationController
 
   def tech_index
-    # @posts = Post.where(tech: true).paginate(:page => params[:page], :per_page => 30).order(created_at: :desc)
-    @posts = Post.where(tech: true).order(created_at: :desc)
+    @posts = Post.where(tech: true).page(params[:page]).order(created_at: :desc)
+    # @posts = Post.where(tech: true).order(created_at: :desc)
+  end
+
+  def poetry_index
+    @posts = Post.where(poem: true).page(params[:page]).order(created_at: :desc)
+    # @posts = Post.where(poem: true).order(created_at: :desc)
   end
 
   def tech
     @post = Post.where(tech: true).find(params[:id])
-  end
-
-  def poetry_index
-    # @posts = Post.where(poem: true).paginate(:page => params[:page], :per_page => 30).order(created_at: :desc)
-    @posts = Post.where(poem: true).order(created_at: :desc)
   end
 
   def poem
